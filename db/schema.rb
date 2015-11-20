420e811136c7d2adc87e24b9fd559b288553c598
Jared Nichols <jnichols300@gmail.com>
2015-11-20 04:09:59 -0500

    bundle heroku test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119065745) do
+ActiveRecord::Schema.define(version: 20151120083448) do
 
   create_table "links", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20151119065745) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
   end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -34,6 +36,7 @@ ActiveRecord::Schema.define(version: 20151119065745) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
